c87cb6a23ff5bdf5153010484ee0181c29a3c7cb
gabrielfernandesdelara <GabrielFdeLara@hotmail.com>
2025-09-04 20:15:04 -0300

    fix: names on js

--- Script/script.js
+++ Script/script.js
@@ -10,7 +10,7 @@ const perk4_img = document.querySelector("#perk4-img");
 
 async function getRandomPerks(role) {
   const jsonPath =
-    role === "Killer" ? "Source/Killer.json" : "Source/Survivor.json";
+    role === "killer" ? "Source/killer.json" : "Source/survivor.json";
   const response = await fetch(jsonPath);
   const perks = await response.json();
   return perks;
